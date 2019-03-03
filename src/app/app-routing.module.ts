79989b94267538dab55d4eb35b2ad93dc02f3538
Anastasia Kutalova <kutalova.a@gmail.com>
2019-03-04 00:31:15 +0200

    added header, inner wrapper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,58 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PageWrapperComponent} from './core/page-wrapper/page-wrapper.component';
+import {Error404Component} from './core/error-pages/error404/error404.component';
+import {UsComponent} from './core/us/us.component';
+import {TariffsComponent} from './core/tariffs/tariffs.component';
+import {FaqComponent} from './core/faq/faq.component';
+import {SafetyComponent} from './core/safety/safety.component';
+import {LegalComponent} from './core/legal/legal.component';
 
-const routes: Routes = [];
+
+
+const AppRouter: Routes = [
+    {
+        path: '', component: PageWrapperComponent, children: [
+            {
+                path: 'onas', component: UsComponent
+            },
+            {
+                path: 'faq', component: FaqComponent
+            },
+            {
+                path: 'tarif', component: TariffsComponent
+            },
+            {
+                path: 'bezopasnost', component: SafetyComponent
+            },
+            {
+                path: 'legal', component: LegalComponent
+            },
+            {
+                path: '404', component: Error404Component
+            },
+            {
+                path: 'user', loadChildren: './modules/user/user.module#UserModule'
+            },
+            {
+                path: 'adv', loadChildren: './modules/adv/adv.module#AdvModule'
+            },
+            {
+                path: '', loadChildren: './modules/home/home.module#HomeModule'
+            }
+        ]
+    },
+    {
+        path: '', pathMatch: 'full', loadChildren: './modules/home/home.module#HomeModule'
+    },
+    {
+        path: '**', redirectTo: '/404'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(AppRouter)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { SharedModule } from './shared.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {SharedModule} from './shared.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PageWrapperComponent} from './core/page-wrapper/page-wrapper.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PageWrapperComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
