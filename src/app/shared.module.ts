ae8761fe0cc338c9cb4050bfcaa4f31ba71325cd
Anastasia Kutalova <kutalova.a@gmail.com>
2019-03-04 02:12:12 +0200

    fixed not working links

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { Error404Component } from './core/error-pages/error404/error404.component';
 import { Error500Component } from './core/error-pages/error500/error500.component';
@@ -10,6 +10,18 @@ import { SafetyComponent } from './core/safety/safety.component';
 import { LegalComponent } from './core/legal/legal.component';
 
 @NgModule({
+    exports: [
+        Error404Component,
+        Error500Component,
+        UsComponent,
+        FaqComponent,
+        TariffsComponent,
+        SafetyComponent,
+        LegalComponent
+    ],
+    imports: [
+        RouterModule
+    ],
     declarations: [
         Error404Component,
         Error500Component,
@@ -19,7 +31,6 @@ import { LegalComponent } from './core/legal/legal.component';
         SafetyComponent,
         LegalComponent
     ],
-    imports: [],
     providers: []
 })
 export class SharedModule { }
